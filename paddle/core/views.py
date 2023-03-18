8771bb77a26d35cc49accd01e06ae29efb5a05f0
kiruhi <peterkiruhi4@gmail.com>
2023-03-18 09:39:33 +0300

    First commit Templates Structure

---
+++ paddle/core/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request,'core/index.html')
+
+
+def contact(request):
+    return render(request, 'core/contact.html')
+
+# Create your views here.
